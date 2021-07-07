ccb12c32e7fafa6e3549d48c1fe93c9dfd89ca45
Lachlanjowett <79903111+lochyj@users.noreply.github.com>
2021-07-07 22:54:36 +1000

    wip weather api page
    idk i just got bored

---
+++ weather/index.js
@@ -0,0 +1,18 @@
+$(document).keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        updateLocation();  
+    }
+});
+function updateLocation(){
+    var uLoc = document.getElementById("input").value;
+    console.log(uLoc)
+    var apiGet = `https://api.openweathermap.org/data/2.5/weather?q=${uLoc}&appid=9ff81a32f74a6ac0ae72420fa124c223&units=metric`;
+    console.log(apiGet);
+    $.getJSON(apiGet, function(data) {
+        var temp = `${data.main.temp}°<br>` 
+        var location = `${data.name}<br>`
+        $("#temp").html(temp);
+        $("#location").html(location);
+    });
+}
