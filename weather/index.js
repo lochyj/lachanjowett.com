93a379b38fb95a6fdd7261fa366dcb704d31d5d0
Lachlanjowett <79903111+lochyj@users.noreply.github.com>
2021-07-20 18:41:53 +0930

    re arranging the file system

--- weather/index.js
+++ weather/index.js
@@ -4,6 +4,16 @@ $(document).keypress(function(event){
         updateLocation();  
     }
 });
+
+let map;
+
+function initMap() {
+map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: updateLocation(lat), lng: updateLocation(lon) },
+    zoom: 8,
+});
+}
+
 function updateLocation(){
     var uLoc = document.getElementById("input").value;
     console.log(uLoc)
@@ -15,6 +25,8 @@ function updateLocation(){
         var locationd = `${data.name}, ${data.sys.country}<br>`
         var location = `${data.name}<br>`
         var windspeed = `Wind speed: ${data.wind.speed} km/h<br>`
+        var lon = data.coord.lon;
+        var lat = data.coord.lat;
         $("#temp").html(temp);
         $("#location").html(locationd);
         $("#heading").html(location);
