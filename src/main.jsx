d9e3e4fcc4ed6cceac5dc711b59350d5ff5f7bcf
Lachlan Jowett <lachlan@jowett.net.au>
2023-12-18 22:18:36 +1100

    working on redesign

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
   BrowserRouter,
-  RouterProvider,
-  Route,
 } from "react-router-dom";
 import App from './App'
 import './index.css'
